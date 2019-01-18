ce27d4cf4241c04305752c94cd6f0df62eb94ded
nicola martorana <nicola.martorana@oddschecker.com>
2019-01-18 08:47:42 +0000

    removed temporary code

--- odds.py
+++ odds.py
@@ -16,23 +16,4 @@ class Odds:
 
     def get_fract_odds(self):
         # returns fractional odds as string
-        if not self._fract_price:
-            # If none string, exit early. Nothing to process
-            return None
-
-        if self._fract_price.count("/") > 1:
-            return None
-
-        accepted_chars = ["/"]
-        value = ""
-
-        for s in self._fract_price:
-            try:
-                num = int(s)
-                value += str(num)
-            except:
-                if s in accepted_chars:
-                    value += s
-                    continue
-                return None
-        return value
+        pass
--- odds_test.py
+++ odds_test.py
@@ -13,11 +13,3 @@ class OddsTest(unittest.TestCase):
     def test_get_fract_odds(self):
         odds = Odds("1/2")
         self.assertEquals(odds.get_fract_odds(), "1/2")
-
-    def test_get_fract_odds_fail(self):
-        odds = Odds("1*2*3")
-        self.assertIsNone(odds.get_fract_odds())
-
-    def test_get_fract_odds_slashes(self):
-        odds = Odds("1/2/3")
-        self.assertIsNone(odds.get_fract_odds())
